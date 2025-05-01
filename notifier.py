c18763ac555a57d10b4350b564ac0761fc75d1f7
Lourojas <lourojas@gmail.com>
2025-05-01 08:09:24 -0400

    notifier.py

--- notifier.py
+++ notifier.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from pushbullet import Pushbullet
 import os
 import time
-import sys
-
-# Redirige todos los print al archivo temporal de logs
-sys.stdout = open('/tmp/log.txt', 'a')
+import chromedriver_autoinstaller
 
 def notify():
     url = "https://m.popmart.com/us/pop-now/set/195"
     api_key = os.getenv("PUSHBULLET_API_KEY")
+    chrome_path = os.getenv("CHROMEDRIVER_PATH")
+
     keywords = ["Buy Multiple Boxes", "Pick One to Shake"]
+
+    # Instala automáticamente el chromedriver compatible
+    chromedriver_autoinstaller.install()
 
     options = Options()
     options.add_argument("--headless")
@@ -20,10 +21,9 @@ def notify():
     options.add_argument("--disable-dev-shm-usage")
 
     try:
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(executable_path=chrome_path, options=options)
         driver.get(url)
-        time.sleep(6)  # Espera para que se cargue el contenido dinámico
-
+        time.sleep(6)  # Espera para que cargue el contenido dinámico
         page_text = driver.page_source
 
         if any(keyword in page_text for keyword in keywords):
@@ -40,6 +40,3 @@ def notify():
 
 if __name__ == "__main__":
     notify()
-
-# Cierra el archivo de log
-sys.stdout.close()
